ae1867975a26f2269c553ba9edbd59076a6cfdaa
Moustapha naouchi <91967764+Moustaphanaouchi@users.noreply.github.com>
2025-01-11 11:23:41 +0300

    Update Server1.java

--- Server1.java
+++ Server1.java
@@ -1,73 +1,286 @@
-// Remove package declaration for testing
+package proje1;
 
 import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server1 {
-    private static final int PORT = 5000; // Sunucu port numarası
-    private static Set<Subscriber> subscribers = Collections.synchronizedSet(new HashSet<>()); // Aboneleri saklamak için senkronize küme
 
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("Server1 is running on port " + PORT); // Sunucunun çalıştığı portu yazdır
+    private static final int PORT_SERVER_1 = 5001;
+    private static final int MAX_CLIENTS = 100;
 
-            // Bağlantılar sürekli dinlenir
-            while (true) {
-                Socket socket = serverSocket.accept(); // Yeni bağlantı kabul et
-                new Thread(new ConnectionHandler(socket)).start(); // Yeni bağlantıyı işlemek için yeni bir iş parçacığı başlat
+    private static List<Boolean> abonelerListesi = new ArrayList<>(MAX_CLIENTS);
+    private static List<Boolean> girisYapanlarListesi = new ArrayList<>(MAX_CLIENTS);
+    private static long lastUpdatedEpochMiliSeconds;
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            abonelerListesi.add(false);
+            girisYapanlarListesi.add(false);
+            lastUpdatedEpochMiliSeconds = 0;
+        }
+        ServerSocket serverSocket = new ServerSocket(PORT_SERVER_1);
+        System.out.println("Server1 is running on port " + PORT_SERVER_1);
+
+        new Thread(() -> {
+            try {
+                listenForConnections(serverSocket);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace(); // Hata durumunda stack trace yazdır
+        }).start();
+        new PingThread("localhost", 5002).start(); // Ping Server2
+        new PingThread("localhost", 5003).start(); // Ping Server3
+    	new Thread(() -> receiveDataFromServer2()).start();
+        new Thread(() -> receiveDataFromServer3()).start();
+
+            
+    }
+
+    private static void listenForConnections(ServerSocket serverSocket) throws IOException {
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            new Thread(() -> handleClient(clientSocket)).start();
         }
     }
 
-    private static class ConnectionHandler implements Runnable {
-        private Socket socket; // Bağlantı soketi
+    public static void handleClient(Socket clientSocket) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
 
-        public ConnectionHandler(Socket socket) {
-            this.socket = socket; // Soketi başlatıcıdan al
+            String message = in.readLine();
+            out.println("İstediğiniz mesaj sunucuya iletildi.");
+
+            if (message != null) {
+                if (message.startsWith("ABONOL")) {
+                	SubscriberABONOL(message, clientSocket, out);
+                } else if (message.startsWith("ABONIPTAL")) {
+                	SubscriberABONIPTAL(message, clientSocket, out);
+                } else if (message.startsWith("GIRIS")) {
+                    handleGIRISMessage(message, clientSocket, out);
+                } else if (message.startsWith("CIKIS")) {
+                    handleCIKISMessage(message, clientSocket, out);
+                } else if (message.equals("SERILESTIRILMIS_NESNE")) {
+                    handleSerilestirilmisNesne(clientSocket, out );
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void SubscriberABONOL(String message, Socket clientSocket, PrintWriter out) throws IOException {
+        int clientNumber = Integer.parseInt(message.split(" ")[1]);
+
+        if (clientNumber >= 0 && clientNumber < MAX_CLIENTS) {
+            if (abonelerListesi.get(clientNumber)) {
+                out.println("Bu client ID için Subscriber zaten var.");
+                sendDataToServers();
+            } else {
+                abonelerListesi.set(clientNumber, true);
+                out.println("Subscriber olundu.");
+                sendDataToServers();
+            }
+        } else {
+            out.println("50 HATA");
+        }
+    }
+
+    private static void SubscriberABONIPTAL(String message, Socket clientSocket, PrintWriter out) throws IOException {
+        int clientNumber = Integer.parseInt(message.split(" ")[1]);
+
+        if (clientNumber >= 0 && clientNumber < MAX_CLIENTS) {
+            if (!abonelerListesi.get(clientNumber)) {
+                out.println("Bu client ID için Subscriber zaten yok.");
+                sendDataToServers();
+            } else {
+                abonelerListesi.set(clientNumber, false);
+                out.println("Subscriber iptal edildi.");
+                sendDataToServers();
+            }
+        } else {
+            out.println("50 HATA");
+        }
+    }
+
+    private static void handleGIRISMessage(String message, Socket clientSocket, PrintWriter out) throws IOException {
+        int clientNumber = Integer.parseInt(message.split(" ")[1]);
+
+        if (clientNumber >= 0 && clientNumber < MAX_CLIENTS) {
+            if (abonelerListesi.get(clientNumber)) {
+                if (girisYapanlarListesi.get(clientNumber)) {
+                    out.println("Bu Client zaten Online.");
+                    sendDataToServers();
+                } else {
+                    girisYapanlarListesi.set(clientNumber, true);
+                    out.println("Online.");
+                    sendDataToServers();
+                }
+            } else {
+                out.println("50 HATA");
+            }
+        } else {
+            out.println("50 HATA");
+        }
+    }
+
+    private static void handleCIKISMessage(String message, Socket clientSocket, PrintWriter out) throws IOException {
+        int clientNumber = Integer.parseInt(message.split(" ")[1]);
+
+        if (clientNumber >= 0 && clientNumber < MAX_CLIENTS) {
+            if (!abonelerListesi.get(clientNumber) || !girisYapanlarListesi.get(clientNumber)) {
+                out.println("Bu Client zaten Offline.");
+                sendDataToServers();
+            } else {
+                girisYapanlarListesi.set(clientNumber, false);
+                out.println("Offline.");
+                sendDataToServers();
+            }
+        } else {
+            out.println("50 HATA");
+        }
+    }
+
+    private static void handleSerilestirilmisNesne(Socket clientSocket, PrintWriter out ) throws IOException {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+            MultiStringArrays receivedAboneler = (MultiStringArrays) ois.readObject();
+
+            receiveDataFromServer2();
+            receiveDataFromServer3();
+            out.println("55 TAMM");
+
+
+            if (receivedAboneler != null && receivedAboneler.getLastUpdatedEpochMiliSeconds() > lastUpdatedEpochMiliSeconds) {
+                lastUpdatedEpochMiliSeconds = receivedAboneler.getLastUpdatedEpochMiliSeconds();
+                out.println("55 TAMM");
+            } else {
+                out.println("99 HATA");
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            out.println("99 HATA");
+        }
+    }
+
+    private static void sendDataToServers() {
+        try {
+            Socket socketServer2 = new Socket("localhost", 5004);
+            Socket socketServer3 = new Socket("localhost", 5008);
+
+            ObjectOutputStream outServer2 = new ObjectOutputStream(socketServer2.getOutputStream());
+            ObjectOutputStream outServer3 = new ObjectOutputStream(socketServer3.getOutputStream());
+
+            MultiStringArrays dataToSend = new MultiStringArrays();
+            dataToSend.setLastUpdatedEpochMiliSeconds(System.currentTimeMillis());
+
+            dataToSend.setAbonelerListesi(abonelerListesi);
+            dataToSend.setGirisYapanlarListesi(girisYapanlarListesi);
+
+            outServer2.writeObject(dataToSend);
+            outServer3.writeObject(dataToSend);
+            System.out.println("Data sent ");
+
+            socketServer2.close();
+            socketServer3.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private static void receiveDataFromServer2() {
+        try (ServerSocket serverSocket = new ServerSocket(5007)) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Thread(() -> handleReceivedData(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void receiveDataFromServer3() {
+        try (ServerSocket serverSocket = new ServerSocket(5006)) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Thread(() -> handleReceivedData(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void handleReceivedData(Socket socket) {
+        try {
+            int senderPort = socket.getPort();
+            if (senderPort != PORT_SERVER_1) {
+                ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+
+                Object receivedObject = input.readObject();
+
+                if (receivedObject instanceof MultiStringArrays) {
+                    MultiStringArrays receivedAboneler = (MultiStringArrays) receivedObject;
+                    System.out.println("Data received :");
+                    
+                    if (receivedAboneler != null && receivedAboneler.getLastUpdatedEpochMiliSeconds() > lastUpdatedEpochMiliSeconds) {
+                        lastUpdatedEpochMiliSeconds = receivedAboneler.getLastUpdatedEpochMiliSeconds();
+                        
+                        List<Boolean> receivedAbonelerList = receivedAboneler.getAbonelerListesi();
+                        List<Boolean> receivedGirisYapanlarList = receivedAboneler.getGirisYapanlarListesi();
+
+                        if (receivedAbonelerList != null && receivedGirisYapanlarList != null) {
+                            abonelerListesi = new ArrayList<>(receivedAbonelerList);
+                            girisYapanlarListesi = new ArrayList<>(receivedGirisYapanlarList);
+                        }
+                        
+                        System.out.println("55 Yerel liste güncellendi TAMM");
+                    } else {
+                        System.out.println("99 HATA");
+                    }
+                }
+            } else {
+                System.out.println("Data received from Server 1. Ignoring...");
+            }
+
+            socket.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static class PingThread extends Thread {
+        private String host;
+        private int port;
+
+        public PingThread(String host, int port) {
+            this.host = host;
+            this.port = port;
         }
 
         public void run() {
-            try (ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-                 ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
 
-                Object inputObject;
-                // Gelen mesajları dinle
-                while ((inputObject = in.readObject()) != null) {
-                    Message message = (Message) inputObject; // Protobuf mesajını al
-                    if (message.getDemand().equals("SUBS")) { // Eğer talep abone olma ise
-                        handleSubscription(message); // Aboneliği işleme al
-                        out.writeObject(new Message("YEP", null)); // Yanıt olarak YEP gönder
-                    } else if (message.getDemand().equals("DEL")) { // Eğer talep abonelik iptali ise
-                        handleUnsubscription(message); // Aboneliği iptal et
-                        out.writeObject(new Message("YEP", null)); // Yanıt olarak YEP gönder
-                    } else if (message.getDemand().equals("CPCTY")) { // Eğer talep kapasite sorgulama ise
-                        handleCapacityQuery(message, out); // Kapasite sorgusunu işle
+            try {
+                while (true) {
+                    try (Socket socket = new Socket(host, port)) {
+                        System.out.println("Pinged " + host + " on port " + port);
+                    } catch (IOException e) {
+                        System.out.println("Ping to " + host + " on port " + port + " failed, retrying...");
+                    }
+
+                    try {
+                        Thread.sleep(10000); // Wait for 10 seconds before retrying
+                    } catch (InterruptedException ie) {
+                        System.out.println("Ping thread interrupted: " + ie.getMessage());
+                        break; // Optional: exit the loop if the thread is interrupted
                     }
                 }
-            } catch (IOException | ClassNotFoundException e) {
-                e.printStackTrace(); // Hata durumunda stack trace yazdır
+            } catch (Exception e) {
+                System.out.println("Unexpected error: " + e.getMessage());
             }
-        }
 
-        private void handleCapacityQuery(Message message, ObjectOutputStream out) throws IOException {
-            // Kapasite sorgusunu işleyin
-            int serverStatus = subscribers.size(); // Abone sayısını al
-            Capacity capacityResponse = new Capacity(1, serverStatus, System.currentTimeMillis()); // Örnek sunucu ID'si
-            out.writeObject(capacityResponse); // Kapasite yanıtını gönder
-        }
 
-        private void handleSubscription(Message message) {
-            // Yeni bir abone oluştur ve aboneleri sakla
-            Subscriber subscriber = new Subscriber(message.getId(), message.getNameSurname(), message.getStartDate(), message.getLastAccessed(), message.getInterests(), message.isOnline());
-            subscribers.add(subscriber);
-        }
-
-        private void handleUnsubscription(Message message) {
-            // Aboneliği iptal et
-            subscribers.removeIf(subscriber -> subscriber.getId() == message.getId());
         }
     }
+
 }
